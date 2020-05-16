b2c6a9db8308da16177dd4211be703ec6b80fd91
Jayram <jayram.rout@hotmail.com>
2020-05-15 20:58:56 -0400

    Hello this my first commit from intellij

---
+++ core-java/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * This is my first Program.
+ * @author jrout
+ */
+public class HelloWorld {
+    /*
+        Signature of the main method..
+     */
+    // This is Single line comment
+    public static void main(String[] args) {
+        System.out.println("Hello world..");
+        System.out.println("This is my output...");
+    }
+
+}
